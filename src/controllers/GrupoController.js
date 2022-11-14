7d9aae30342f0fc98f90f04c290660a370f35357
Alexandre Matheus <alexandrefenix1@hotmail.com>
2022-11-14 19:08:21 -0300

    Update GrupoController.js

--- src/controllers/GrupoController.js
+++ src/controllers/GrupoController.js
@@ -42,3 +42,23 @@ module.exports = {
     }
 }
 
+const express = require('express');
+const router = express.Router();
+
+
+const GrupoService = require('../services/GrupoService');
+
+
+router.route('/Grupo')
+      .get(GrupoService.ListarGrupo)
+      .post(GrupoService.CrearGrupo);
+
+router.route('/Grupo/:id')
+      .get(GrupoService.obtenerGrupoId)
+      .put(GrupoService.actualizarGrupo)
+      .delete(GrupoService.eliminarGrupo);      
+
+
+     
+     
+ module.exports = router;
